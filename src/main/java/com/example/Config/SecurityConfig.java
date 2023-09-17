86e8d1fdce5372fa08824e97e7534242f0911ebd
okano sanaka <sanaka.7hex.bss@gmail.com>
2023-09-18 01:22:01 +0900

    確認お願いいたします

---
+++ src/main/java/com/example/Config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.Config;
+
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+
+// Spring Securityの設定を有効にします
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		//認可に関する設定
+		http.authorizeRequests()
+		.antMatchers("/loginForm").permitAll()  // /loginFormは全ユーザからのアクセスを許可
+        .anyRequest().authenticated();   // 許可した項目以外は認証を求める
+		
+		 //ログイン処理
+	    http.formLogin()
+	        .loginProcessingUrl("/login") // ログイン処理のパス
+	        .loginPage("/loginForm") // ログインページの指定
+	        .usernameParameter("email") // ログインページのメールアドレス
+	        .passwordParameter("password") // ログインページのパスワード
+	        .defaultSuccessUrl("/home", true) // ログイン成功後のパス
+	        .failureUrl("/loginForm?error"); // ログイン失敗時のパス
+	    
+	    //ログアウト処理
+	    http.logout()
+	    .logoutUrl("/logout") //ログアウト処理のパス
+	    .logoutSuccessUrl("/loginForm") //ログアウト成功後のパス
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override  // WebSecurity型の引数を持ったconfigure()を追加します
+    public void configure(WebSecurity web) throws Exception {
+		 web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+	}
+}
