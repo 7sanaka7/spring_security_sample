dee691e3c9346f1624830c0b193834057464d66f
okano sanaka <sanaka.7hex.bss@gmail.com>
2023-09-19 19:10:38 +0900

    Lesson5実装途中。

--- src/main/java/com/example/Config/SecurityConfig.java
+++ src/main/java/com/example/Config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.example.Config;
 
 import org.springframework.context.annotation.Bean;
-
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // Spring Securityの設定を有効にします
 @EnableWebSecurity
